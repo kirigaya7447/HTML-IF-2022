58db71341794c0fade7615aff476137a000445a8
kirigaya7447 <joaoh2915@gmail.com>
2024-10-31 22:29:45 -0300

    Consertando alguns códigos

--- Arquivos em .html/Manipulando valores em JavaScript/Manipulando.js
+++ Arquivos em .html/Manipulando valores em JavaScript/Manipulando.js
@@ -1,8 +1,11 @@
 function frase(){
     var fra;
     var co;
+    var ca;
     fra = document.getElementById("i1").value;
+    ca = document.getElementById("i2").value;
     document.getElementById("i2").value = fra;
+    document.getElementById("i1").value = ca;
     co = document.getElementById("pa").innerHTML;
     document.getElementById("di").innerHTML = co;
 }
